754e0c57372d84845e917f7ce5303d7f2a13ca87
James Foucar <jgfouca@sandia.gov>
2019-08-10 16:54:10 -0600

    Add files

---
+++ ice_flux.F90
@@ -0,0 +1,31 @@
+module ice_flux
+
+  implicit none
+  save
+
+  real, dimension (1,2,3) :: &
+       daidtd  , &
+       dvidtd
+
+  ! The presense of this segment breaks the detection of the ice_state dependency
+  real, dimension(1,2,3) :: &
+#ifdef FOO
+       baz, &
+#endif
+#ifdef BAR
+       bla, &
+#endif
+       goo
+  ! end broken segment
+
+contains
+
+  subroutine init_history_dyn
+    use ice_state, only: aice, vice
+
+    daidtd  (:,:,:) = aice(:,:,:)
+    dvidtd  (:,:,:) = vice(:,:,:)
+
+  end subroutine init_history_dyn
+
+end module ice_flux
---
+++ ice_state.F90
@@ -0,0 +1,10 @@
+module ice_state
+
+  implicit none
+  save
+
+  real, dimension(1,2,3) :: &
+       aice  , & ! concentration of ice
+       vice
+
+end module ice_state
